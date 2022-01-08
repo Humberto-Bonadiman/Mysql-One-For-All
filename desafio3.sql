0fb59f23df9d7713a2fade1279a877f3d296d8e7
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-01-08 17:53:24 -0400

    Finalizado requisito 3

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT u.usuario AS usuario,
+	COUNT(h.usuario_id) AS qtde_musicas_ouvidas,
+    ROUND(SUM(r.duracao_segundos/60), 2) AS total_minutos
+FROM SpotifyClone.usuarios AS u
+INNER JOIN SpotifyClone.historicos AS h
+ON u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone.reproducoes AS r
+ON h.reproducao_id = r.reproducao_id
+GROUP BY u.usuario
+ORDER BY u.usuario;
